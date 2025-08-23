81f188151c51984895c315ea552be5deeb6fd90c
Drei08 <andrei_rheivjarth@hotmail.com>
2025-08-22 23:55:53 -0300

    criado resultado na tela

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const tagVoce = document.getElementById('img-voce');
 const tagComputador = document.getElementById('img-computador');
 const tagResult = document.getElementById('result');
 
+const tagSomaJ = document.getElementById('somaJ');
+const tagSomaC = document.getElementById('somaC');
+const tagEmp = document.getElementById('emp');
+
+let somaJ = 0;
+let emp = 0;
+let somaC = 0;
+
 function jogar(escolhaUsuario) {
 
     const opcoes = ['pedra', 'papel', 'tesoura'];
@@ -13,21 +21,31 @@ function jogar(escolhaUsuario) {
    
     tagVoce.src = `${escolhaUsuario}.png`;
     tagComputador.src = `${escolhaComputador}.png`;
+
+    tagSomaJ.textContent = somaJ;
+    tagSomaC.textContent = somaC;
+    tagEmp.textContent = emp;
+
 }
 
 function verificarResultado(jogador, computador) {
 
-    if (jogador === computador)
+    if (jogador === computador){
+        
+         emp += 1;
          return "Empate!";
+    }
 
     else if 
         ((jogador === "pedra" && computador === "tesoura") ||
         (jogador === "papel" && computador === "pedra") ||
         (jogador === "tesoura" && computador === "papel")){
 
+        somaJ += 1;
         return "Você ganhou!";
     }
 
     else
-    return "Computador ganhou!";
+        somaC += 1;
+        return "Computador ganhou!";
 }
